c6422f94d4848080a82b7a10bd0ce5bc3bfc990a
Jerry Wang <moonspirit.wcy@gmail.com>
2020-05-11 00:29:25 +0800

    check in java gradle project

---
+++ src/main/java/LeetCodeJavaPractice/TownJudge.java
@@ -0,0 +1,36 @@
+package LeetCodeJavaPractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TownJudge {
+
+	public int findJudge(int N, int[][] trust) {
+		Map<Integer, Integer> beTrusted = new HashMap<>();
+		Map<Integer, Integer> trustsomeone = new HashMap<>();
+		for (int i = 0 ; i < trust.length; i++) {
+			if (beTrusted.containsKey(trust[i][1])) {
+				beTrusted.put(trust[i][1], beTrusted.get(trust[i][1])+1);
+			} else {
+				beTrusted.put(trust[i][1], 1);
+			}
+			if (trustsomeone.containsKey(trust[i][0])) {
+				trustsomeone.put(trust[i][0], trustsomeone.get(trust[i][0])+1);
+			} else {
+				trustsomeone.put(trust[i][0], 1);
+			}
+		}
+		int judge = -1;
+		for (Map.Entry<Integer, Integer> entry : beTrusted.entrySet()) {
+			if (entry.getValue() == N-1) {
+				if (!trustsomeone.containsKey(entry.getKey())) {
+					judge = entry.getKey();
+				}
+			}
+		}
+		if (trust.length == 0 && N == 1) {
+			judge = 1;
+		}
+		return judge;    
+	}
+}
---
+++ src/test/java/LeetCodeJavaPractice/TownJudgeTest.java
@@ -0,0 +1,16 @@
+package LeetCodeJavaPractice;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class TownJudgeTest {
+
+    @Test public void testSomeLibraryMethod() {
+        TownJudge townJudge = new TownJudge();
+        assertEquals(3, townJudge.findJudge(4, new int[][] {{1,3},{1,4},{2,3},{2,4},{4,3}}));
+        assertEquals(1, townJudge.findJudge(1, new int[][] {}));
+    }
+}
